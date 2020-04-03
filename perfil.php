4c4f0b5b8897d461e4f52b2ff313ab667c03937a
alam <20175131@itla.edu.do>
2020-04-03 01:36:34 -0400

    Carta-Perfil

---
+++ perfil.php
@@ -0,0 +1,165 @@
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Goudy Bookletter 1911' rel='stylesheet'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link href="/open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
+
+
+</head>
+<style>
+    .fondo {
+        background-image: url("IMAGENES/horseman.jpg");
+        background-size: cover;
+
+
+
+    }
+
+    .body {
+        font-family: Josefin Sans;
+        background-color: #E7E5DF;
+    }
+
+
+    img {
+
+        width: 60px;
+    }
+
+    .boton {
+        width: 30px;
+        height: 30px;
+        background-color: #03800b;
+        margin: 5px;
+        padding: 10px;
+        -webkit-border-radius: 50px;
+        -moz-border-radius: 50px;
+        border-radius: 50px;
+        font-size: 11px;
+        line-height: 32px;
+        text-transform: uppercase;
+        float: left;
+    }
+</style>
+
+
+<?php include 'navbar.php'; ?>
+
+<body class="body" >
+    <div class="img-fluid" style="width: 100%;height: 300px;  ; background-color:black; filter:brightness(0.4);">
+
+
+    </div>
+
+
+    </div>
+    <div class="container elevation" style="width:90%">
+        <div class="row">
+            <div class="col-4">
+                <div class="card elevation" style="width: 18rem;margin-top: -200px; background-color: #E7E5DF;">
+
+                    <div class="card-head cabeza">
+                        <img src="IMAGENES/fondo3.jpg" style="width: 100%; height: auto;">
+                        <button class="elevation btn " style="margin-top:-50px;margin-left:30px;
+                       background-color:#fff;border-radius:50px;color:#000;font-size:10px;width:60%"> Follow me
+
+                        </button>
+
+                    </div>
+                    <div class="card-body">
+                        <b style="color:grey">About</b>
+                        <p class="card-text" style="font-size: 12px;color: grey;margin-top: 10px;">Throughout this book
+                            are various writing, speaking, and listening ideas, such as annotation and peer
+                            conversations, questioning techniques, dialectical journals, graphic organizers,</p>
+                        <div class="row" style="margin-top: 30px;">
+                            <div class="col-8">
+
+                                <p style="color:grey ;">Follwers</p>
+
+                            </div>
+                            <div class="col-4">
+
+                                200k
+                            </div>
+                        </div>
+                        <div class="row" style="margin-top: -20px;">
+                            <div class="col-8">
+
+                                <p style="color:grey ;"> Follwers</p>
+
+                            </div>
+                            <div class="col-4">
+
+                                200k
+                            </div>
+                        </div>
+                        <div class="row" style="margin-top: -20px;">
+                            <div class="col-8">
+
+                                <p style="color:grey ;">Follwers</p>
+
+                            </div>
+                            <div class="col-4">
+
+                                200k
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+
+            </div>
+            <div class="col-6" style="margin-top:-90px">
+
+                <h5 style="color:#E7E5DF ;">
+                    Alamo Mena
+                </h5>
+
+                <p style="color:grey ;">
+                    Santo domingo, Republica Dominicana
+                </p>
+            </div>
+<div class="col" style="margin-left: -40rem;">
+    <div class="card elevation" style="width: 50rem;height: 13rem;">
+        <div class="card-body">
+            <div class="row">
+
+                <div class="col-lg-8 col-sm-3">
+                    <h5 style="color:grey ;">
+                        Alamo Mena
+                    </h5>
+    
+                    <p style="color:grey ;">
+                        Santo domingo, Republica Dominicana
+                    </p>
+                    
+        
+                </div>
+             <div class="col-2">
+                 <img src="IMAGENES/like.png"style="width:20px; "><b style="color:grey ;font-size: 10px; margin-left: 10px;">20</b> 
+                
+             </div>
+             <div class="col-2"style="margin-left:-5px">
+                <img src="IMAGENES/share.png"style="width:20px;margin-left:-40px "><b style="color:grey ;font-size: 10px; margin-left: 10px;">share</b> 
+               
+            </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+        </div>
+    </div>
+</body>
+
+</html>
